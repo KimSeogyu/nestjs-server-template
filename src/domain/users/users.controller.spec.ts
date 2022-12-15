7f1beb4b36c970193c24b55b173aeada7acfea1c
김서규 <asap0208@coinone.com>
2022-12-15 11:24:07 +0900

    test: test code 안되는 문제 해결

--- src/domain/users/users.controller.spec.ts
+++ src/domain/users/users.controller.spec.ts
@@ -1,6 +1,10 @@
+import { DatabaseModule } from '@app/database/database.module';
 import { Test, TestingModule } from '@nestjs/testing';
 import { expect } from 'chai';
+import { DataSource } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
 import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
@@ -8,8 +12,18 @@ describe('UsersController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [DatabaseModule],
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        UsersRepository,
+        {
+          provide: 'USER_REPOSITORY',
+          useFactory: (dataSource: DataSource) =>
+            dataSource.getRepository(UserEntity),
+          inject: ['DATA_SOURCE'],
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- src/domain/users/users.service.spec.ts
+++ src/domain/users/users.service.spec.ts
@@ -1,5 +1,10 @@
+import { DatabaseModule } from '@app/database/database.module';
 import { Test, TestingModule } from '@nestjs/testing';
 import { expect } from 'chai';
+import { DataSource } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
+import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
@@ -7,7 +12,18 @@ describe('UsersService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      imports: [DatabaseModule],
+      controllers: [UsersController],
+      providers: [
+        UsersService,
+        UsersRepository,
+        {
+          provide: 'USER_REPOSITORY',
+          useFactory: (dataSource: DataSource) =>
+            dataSource.getRepository(UserEntity),
+          inject: ['DATA_SOURCE'],
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
