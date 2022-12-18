543b9f123f0b7ef9b27d63b87599061763bd6187
seogyugim <asap0208@gmail.com>
2022-12-19 01:22:43 +0900

    feat: schema should be strict

--- src/utils/config.schema.ts
+++ src/utils/config.schema.ts
@@ -1,17 +1,19 @@
 import { z } from 'zod';
 
-export const ConfigSchema = z.object({
-  AUTH_USERNAME: z.string(),
-  AUTH_PASSWORD: z.string(),
-  APP_VERSION: z.string(),
-  DATABASE_HOST: z.string(),
-  DATABASE_PORT: z.number(),
-  DATABASE_USERNAME: z.string(),
-  DATABASE_PASSWORD: z.string(),
-  DATABASE_SCHEMA: z.string(),
-  JWT_SECRET: z.string(),
-  JWT_EXPIRE_TIME: z.string(),
-  SLACK_WEBHOOK_URL: z.string(),
-});
+export const ConfigSchema = z
+  .object({
+    AUTH_USERNAME: z.string(),
+    AUTH_PASSWORD: z.string(),
+    APP_VERSION: z.string(),
+    DATABASE_HOST: z.string(),
+    DATABASE_PORT: z.number(),
+    DATABASE_USERNAME: z.string(),
+    DATABASE_PASSWORD: z.string(),
+    DATABASE_SCHEMA: z.string(),
+    JWT_SECRET: z.string(),
+    JWT_EXPIRE_TIME: z.string(),
+    SLACK_WEBHOOK_URL: z.string(),
+  })
+  .strict();
 
 export type IConfig = z.infer<typeof ConfigSchema>;
