7ff3682d18f8341b9cc71172ffa4d2514bb67b7c
seogyugim <asap0208@gmail.com>
2022-12-19 01:10:48 +0900

    feat: apply zod to validate config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,18 @@
+import { ConfigSchema, IConfig } from '@app/utils/config.schema';
 import { registerAs } from '@nestjs/config';
 
-const DefaultConfig = registerAs('', async () => ({
-  AUTH_USERNAME: process.env.AUTH_USERNAME ?? 'user',
-  AUTH_PASSWORD: process.env.AUTH_PASSWORD ?? 'pass',
-}));
+const parsedConfig = ConfigSchema.parse({
+  AUTH_USERNAME: process.env.AUTH_USERNAME?.toString() || 'example',
+  AUTH_PASSWORD: process.env.AUTH_PASSWORD?.toString() || 'example',
+  APP_VERSION: process.env.APP_VERSION?.toString() || 'example',
+  DATABASE_HOST: process.env.DATABASE_HOST?.toString() || 'example',
+  DATABASE_PORT: Number(process.env.DATABASE_PORT) || 3306,
+  DATABASE_USERNAME: process.env.DATABASE_USERNAME?.toString() || 'example',
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD?.toString() || 'example',
+  DATABASE_SCHEMA: process.env.DATABASE_SCHEMA?.toString() || 'example',
+  JWT_SECRET: process.env.JWT_SECRET?.toString() || 'example',
+  JWT_EXPIRE_TIME: process.env.JWT_EXPIRE_TIME?.toString() || 'example',
+  SLACK_WEBHOOK_URL: process.env.SLACK_WEBHOOK_URL?.toString() || 'example',
+});
 
-export default DefaultConfig;
+export default registerAs('', async (): Promise<IConfig> => parsedConfig);
