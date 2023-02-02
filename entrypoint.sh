e883dc26a0b3032ae81e27d5eb53e5cc7a4b1915
seogyugim <asap0208@gmail.com>
2023-02-02 11:52:56 +0900

    feat: 엔트리포인트 config 환경변수

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 # Install config Example
 # echo "Download ${NODE_ENV}.config..."
-# curl -sL https://config.sh -o ./dist/config.json
+# curl -sL https://s3bucket.com/metaverse2/${NODE_ENV}/config.json -o ./dist/config.json
 
 node dist/main.js
