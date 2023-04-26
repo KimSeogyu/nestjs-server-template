78f68730e30368bf3c06049edb4c78fb3684973f
kimseogyu <asap0208@coinone.com>
2023-04-26 16:15:23 +0900

    feat: upgrade main.ts logic

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import * as process from 'process';
+import { z } from 'zod';
 
-const APP = process.env.APP || 'api';
+const validateApp = z.enum(['api', 'cli']).safeParse(process.env.APP);
+if (!validateApp.success) {
+  console.error(
+    `process.env.APP should be one of api or cli. You gave ${process.env.APP}`,
+  );
+} else {
+  const app = validateApp.data;
+  const { bootstrap } = await import(`./applications/${app}/main.js`);
 
-const { bootstrap } = await import(`./applications/${APP}/main.js`);
-
-bootstrap();
+  bootstrap();
+}
